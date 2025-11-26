7f68fd8f9676ec7ea71a75ba3d5f8bf502f512e6
Octavian Suciu <ceva@ceva.local>
2025-11-26 19:15:39 +0200

    add h2 +fix

---
+++ h2-backup-by-date/create_bk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+toBackup=$1
+backuptime=$(date +"%Y-%m-%d_%H-%M-%S")
+
+backup_by_date() {
+ if [[ -e "$1" ]]; then
+ name=$(basename "$1")
+ #cp -ar "$1" "backup_$name_$backuptime" #only copy
+ tar -czvf "backup_$name_$backuptime.tgz" "$1" #archive
+ else
+ echo "No $name found!"
+ fi
+}
+
+backup_by_date $1
